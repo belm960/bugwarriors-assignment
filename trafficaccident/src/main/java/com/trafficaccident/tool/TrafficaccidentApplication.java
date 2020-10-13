404b0449c581efbe7c19c327973b6794acb5ac5c
belm960 <samuelbefikadu10@gmail.com>
2020-10-13 08:19:04 -0400

    this is samuel

---
+++ trafficaccident/src/main/java/com/trafficaccident/tool/TrafficaccidentApplication.java
@@ -0,0 +1,13 @@
+package com.trafficaccident.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficaccidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficaccidentApplication.class, args);
+	}
+
+}
